3653e9bb5eab528805c1ae1e4580af480975feb5
Jaap de Haan <jaap.dehaan@alpine.de>
2019-01-13 10:50:13 +0100

    #15, #16: Preparing for adding more handlers (dmn, cmmn)

--- script.js
+++ script.js
@@ -1,36 +1,45 @@
 jQuery(document).ready(function() {
-    jQuery("textarea[id^=__bpmnio_]").each(function(i, tag) { try {
-    var xml = jQuery(tag).text();
-    xml = decodeURIComponent(escape(window.atob(xml)));
-  	var id = jQuery(tag).attr('id');
-	// avoid doing it twice
-	jQuery(tag).removeAttr('id');
+  jQuery("textarea[id^=__bpmn_js_]").each(function(i, tag) {
+    try {
+      var xml = jQuery(tag).text();
+      xml = decodeURIComponent(escape(window.atob(xml)));
+      var id = jQuery(tag).attr("id");
+      // avoid doing it twice
+      jQuery(tag).removeAttr("id");
 
-	// bundle exposes the viewer / modeler via the BpmnJS variable
-  	var BpmnViewer = window.BpmnJS;
-  	var containerdiv = document.createElement('div');
-  	containerdiv.className = "canvas";
-  	jQuery(tag).parent().append(containerdiv);
-  	var viewer = new BpmnViewer({ container: containerdiv });
-	viewer.importXML(xml, function(err) {
-	    if (err) {
-	        containerdiv.text = err;
-      	        console.log('error rendering', err);
-    	    } else {
-                var canvas = viewer.get('canvas');
-                var bboxViewport = canvas.getDefaultLayer().getBBox();
-                var bboxSvg = canvas.getSize();
-                canvas.viewbox({ x: bboxViewport.x, y: bboxViewport.y, width: bboxSvg.width, height: bboxSvg.height });
-                var height = bboxViewport.height + 4;
-                // hack: adjust the div height because it doesn't automatically.
-                containerdiv.style.height = "" + height + 'px';
-                containerdiv.style.width = "" + bboxViewport.width + 'px';
-                // Fix #3 by introducing a small space to allow clicks.
-                containerdiv.style.marginRight = "32px";
-            }
-	});
-	jQuery(tag).remove();
-    }catch(err){
-        console.warn(err.message);
-    }});
+      // bundle exposes the viewer / modeler via the BpmnJS variable
+      var BpmnViewer = window.BpmnJS;
+      var containerdiv = document.createElement("div");
+      containerdiv.className = "canvas";
+      jQuery(tag)
+        .parent()
+        .append(containerdiv);
+      var viewer = new BpmnViewer({ container: containerdiv });
+      viewer.importXML(xml, function(err) {
+        if (err) {
+          containerdiv.text = err;
+          console.log("error rendering", err);
+        } else {
+          var canvas = viewer.get("canvas");
+          var bboxViewport = canvas.getDefaultLayer().getBBox();
+          var bboxSvg = canvas.getSize();
+          canvas.viewbox({
+            x: bboxViewport.x,
+            y: bboxViewport.y,
+            width: bboxSvg.width,
+            height: bboxSvg.height
+          });
+          var height = bboxViewport.height + 4;
+          // hack: adjust the div height because it doesn't automatically.
+          containerdiv.style.height = "" + height + "px";
+          containerdiv.style.width = "" + bboxViewport.width + "px";
+          // Fix #3 by introducing a small space to allow clicks.
+          containerdiv.style.marginRight = "32px";
+        }
+      });
+      jQuery(tag).remove();
+    } catch (err) {
+      console.warn(err.message);
+    }
+  });
 });
--- syntax.php
+++ syntax.php
@@ -62,7 +62,7 @@ class syntax_plugin_bpmnio extends DokuWiki_Syntax_Plugin
                     break;
 
                 case DOKU_LEXER_UNMATCHED:
-                    $bpmnid = uniqid('__bpmnio_');
+                    $bpmnid = uniqid('__bpmn_js_');
                     $sectionEditData = ['target' => 'plugin_bpmnio'];
                     if (!defined('SEC_EDIT_PATTERN')) {
                         // backwards-compatibility for Frusterick Manners (2017-02-19)
@@ -71,10 +71,10 @@ class syntax_plugin_bpmnio extends DokuWiki_Syntax_Plugin
                     $class = $renderer->startSectionEdit($data[$pos], $sectionEditData);
                     $renderer->doc .= '<div class="' . $class . '">';
                     $renderer->doc .= '<div style="overflow:auto;">';
-                    $renderer->doc .= '<textarea class="bpmnio_data" id="' . $bpmnid . '" style="visibility:hidden;">';
+                    $renderer->doc .= '<textarea class="bpmn_js_data" id="' . $bpmnid . '" style="visibility:hidden;">';
                     $renderer->doc .= trim($match);
                     $renderer->doc .= '</textarea>';
-                    //$renderer->doc .= '<div class="bpmnio_canvas" id="'.$bpmnid.'"></div>';
+                    //$renderer->doc .= '<div class="bpmn_js_canvas" id="'.$bpmnid.'"></div>';
                     $renderer->doc .= '</div>';
                     $renderer->doc .= '</div>';
                     $renderer->finishSectionEdit($data[$end]);
