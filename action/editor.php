d4abf4f00ed385621d0aefbc15088975099af248
Jaap de Haan <jaap.dehaan@alpsalpine.com>
2022-07-27 08:14:15 +0200

    feat: Integrate the BPMN editor (#32)

--- action/editor.php
+++ action/editor.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @license    See LICENSE file
  * @author     Jaap de Haan <jaap.dehaan@color-of-code.de>
@@ -8,11 +9,19 @@
 // * https://www.dokuwiki.org/devel:section_editor
 // * https://www.dokuwiki.org/devel:releases:refactor2021
 
+use dokuwiki\Form\Form;
+use dokuwiki\Utf8;
+
 class action_plugin_bpmnio_editor extends DokuWiki_Action_Plugin
 {
     public function register(Doku_Event_Handler $controller)
     {
         $controller->register_hook('HTML_SECEDIT_BUTTON', 'BEFORE', $this, 'secedit_button');
+
+        $controller->register_hook('EDIT_FORM_ADDTEXTAREA', 'BEFORE', $this, 'handle_form');
+        $controller->register_hook('HTML_EDIT_FORMSELECTION', 'BEFORE', $this, 'handle_form');
+
+        $controller->register_hook('ACTION_ACT_PREPROCESS', 'BEFORE', $this, 'handle_post');
     }
 
     function secedit_button(Doku_Event $event)
@@ -20,6 +29,66 @@ class action_plugin_bpmnio_editor extends DokuWiki_Action_Plugin
         if ($this->_shall_ignore($event)) return;
 
         $event->data['name'] = $this->getLang('edit_diagram');
+    }
+
+    function handle_form(Doku_Event $event)
+    {
+        if ($this->_shall_ignore($event)) return;
+
+        global $TEXT;
+        global $RANGE;
+        global $INPUT;
+
+        if (!$RANGE) {
+            // section editing failed, use default editor instead
+            $event->data['target'] = 'section';
+            return;
+        }
+
+        $event->stopPropagation();
+        $event->preventDefault();
+
+        $form = &$event->data['form'];
+        $data = base64_encode($TEXT);
+
+        $this->_addHidden($form, 'plugin_bpmnio_data', $data);
+        $this->_addHTML($form, <<<HTML
+            <div class="plugin-bpmnio" id="plugin_bpmnio__editor">
+                <div class="bpmn_js_data">{$data}</div>
+                <div class="bpmn_js_canvas">
+                    <div class="bpmn_js_container"></div>
+                </div>
+                </div>
+            </div>
+            HTML);
+
+        // used during previews
+        $this->_addHidden($form, 'target', 'plugin_bpmnio');
+        $this->_addHidden($form, 'range', $RANGE);
+    }
+
+    function handle_post(Doku_Event $event)
+    {
+        global $TEXT;
+        global $INPUT;
+
+        if (!$INPUT->post->has('plugin_bpmnio_data')) return;
+
+        $TEXT = base64_decode($INPUT->post->str('plugin_bpmnio_data'));
+    }
+
+    private function _addHidden($form, $field, $data)
+    {
+        if (is_a($form, Form::class)) { // $event->name is EDIT_FORM_ADDTEXTAREA
+            $form->setHiddenField($field, $data);
+        } else { // $event->name is HTML_EDIT_FORMSELECTION
+            $form->addHidden($field, $data);
+        }
+    }
+
+    private function _addHTML($form, $data)
+    {
+        $form->addHTML($data);
     }
 
     private function _shall_ignore(Doku_Event $event)
--- script/bpmnio_render.js
+++ script/bpmnio_render.js
@@ -6,9 +6,13 @@ async function renderDiagram(xml, container, viewer, computeSizeFn) {
     try {
         const result = await viewer.importXML(xml);
         const { warnings } = result;
-        console.log(warnings);
+        if (warnings?.length > 0) console.log(warnings);
+
+        if (!computeSizeFn) return;
 
         const size = computeSizeFn(viewer);
+        if (!size) return;
+
         container.style.height = `${size.height}px`;
         container.style.width = `max(100%,${size.width}px)`;
     } catch (err) {
@@ -55,14 +59,11 @@ function computeDmnDiagramSize(viewer) {
             height: bboxViewport.height,
         };
     }
-    return {
-        width: 0,
-        height: 0,
-    };
+    return undefined;
 }
 
 async function renderBpmnDiagram(xml, container) {
-    const BpmnViewer = window.BpmnJS;
+    const BpmnViewer = window.BpmnJS.Viewer;
     const viewer = new BpmnViewer({ container });
 
     renderDiagram(xml, container, viewer, computeBpmnDiagramSize);
@@ -73,6 +74,29 @@ async function renderDmnDiagram(xml, container) {
     const viewer = new DmnViewer({ container });
 
     renderDiagram(xml, container, viewer, computeDmnDiagramSize);
+}
+
+async function transferXmlToForm(editor) {
+    try {
+        const options = { format: true };
+        const result = await editor.saveXML(options);
+        const { xml } = result;
+        if (xml.length > 0) {
+            jQuery('input[name="plugin_bpmnio_data"]').val(window.btoa(xml));
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function renderEditor(xml, container) {
+    const BpmnEditor = window.BpmnJS;
+    const editor = new BpmnEditor({ container });
+
+    editor.on("commandStack.changed", () => transferXmlToForm(editor));
+
+    renderDiagram(xml, container, editor, null);
+    transferXmlToForm(editor);
 }
 
 function safeRender(tag, fn) {
@@ -97,5 +121,8 @@ jQuery(document).ready(function () {
     );
     jQuery("div[id^=__dmn_js_]").each((_, tag) =>
         safeRender(tag, renderDmnDiagram)
+    );
+    jQuery("div[id=plugin_bpmnio__editor]").each((_, tag) =>
+        safeRender(tag, renderEditor)
     );
 });
