1624092f57fa06f1d4306f740a0b3e7a5c33281a
Jaap de Haan <ColorOfCode@googlemail.com>
2023-04-15 17:35:44 +0000

    php concat

--- syntax/bpmnio.php
+++ syntax/bpmnio.php
@@ -88,7 +88,7 @@ class syntax_plugin_bpmnio_bpmnio extends DokuWiki_Syntax_Plugin
         if ($mode == 'xhtml' || $mode == 'odt') {
             switch ($state) {
                 case DOKU_LEXER_ENTER:
-                    $bpmnid = "__{$type}_js_"+$posStart;
+                    $bpmnid = "__{$type}_js_" . $posStart;
                     $renderer->doc .= <<<HTML
                         <div class="plugin-bpmnio" id="{$bpmnid}">
                         HTML;
