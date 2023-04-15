72082d5eae265a148723b029fd28acfc86660ba5
Jaap de Haan <ColorOfCode@googlemail.com>
2023-04-15 16:02:55 +0000

    feat: Add github workflow for unit test

--- syntax/bpmnio.php
+++ syntax/bpmnio.php
@@ -51,7 +51,7 @@ class syntax_plugin_bpmnio_bpmnio extends DokuWiki_Syntax_Plugin
     public function handle($match, $state, $pos, Doku_Handler $handler)
     {
         switch ($state) {
-            case DOKU_LEXER_ENTER :
+            case DOKU_LEXER_ENTER:
                 $matched = '';
                 preg_match('/<bpmnio type="(\w+)">/', $match, $matched);
                 $this->$type = $matched[1] ?? 'bpmn';
