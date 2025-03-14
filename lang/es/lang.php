6d3186463979ba92915e19349ea321d860a11bdb
Jaap de Haan <jaap.dehaan@alpsalpine.com>
2025-03-14 13:42:37 +0000

    fix: Add translations for es, it, jp, nl

---
+++ lang/es/lang.php
@@ -0,0 +1,5 @@
+<?php
+$lang['picker']          = 'Diagramas BPMNIO';
+$lang['bpmn_add']        = 'Agregar un diagrama BPMN';
+$lang['dmn_add']         = 'Agregar un diagrama DMN';
+$lang['edit_diagram']    = 'Editar diagrama';
---
+++ lang/jp/lang.php
@@ -0,0 +1,5 @@
+<?php
+$lang['picker']          = 'BPMNIO 図';
+$lang['bpmn_add']        = 'BPMN図を追加';
+$lang['dmn_add']         = 'DMN図を追加';
+$lang['edit_diagram']    = '図を編集';
