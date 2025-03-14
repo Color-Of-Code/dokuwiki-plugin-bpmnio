51dc74aecfb8412ab5d35db6a2eb5ba6487b40e8
Jaap de Haan <ColorOfCode@googlemail.com>
2025-03-14 14:43:53 +0100

    fix: Add translations for es, it, jp, nl (#51)

---
+++ lang/it/lang.php
@@ -0,0 +1,5 @@
+<?php
+$lang['picker']          = 'Diagrammi BPMNIO';
+$lang['bpmn_add']        = 'Aggiungi un diagramma BPMN';
+$lang['dmn_add']         = 'Aggiungi un diagramma DMN';
+$lang['edit_diagram']    = 'Modifica diagramma';
---
+++ lang/nl/lang.php
@@ -0,0 +1,5 @@
+<?php
+$lang['picker']          = 'BPMNIO Diagrammen';
+$lang['bpmn_add']        = 'Voeg een BPMN-diagram toe';
+$lang['dmn_add']         = 'Voeg een DMN-diagram toe';
+$lang['edit_diagram']    = 'Bewerk diagram';
