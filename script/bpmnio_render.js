415bab93bfdee35ec27d4bd9bf9f6b467e748716
Jaap de Haan <ColorOfCode@googlemail.com>
2025-03-14 18:08:19 +0100

    fix: DMN modeler (#52)
    * fix: DMN modeler
    * chore: bump version

--- script/bpmnio_render.js
+++ script/bpmnio_render.js
@@ -1,12 +1,14 @@
 function extractXml(data) {
-    return new TextDecoder().decode(Uint8Array.from(atob(data), c => c.charCodeAt(0)));
+    return new TextDecoder()
+        .decode(Uint8Array.from(atob(data), (c) => c.charCodeAt(0)))
+        .trim();
 }
 
 async function renderDiagram(xml, container, viewer, computeSizeFn) {
     try {
         const result = await viewer.importXML(xml);
         const { warnings } = result;
-        if (warnings?.length > 0) console.log(warnings);
+        if (warnings?.length > 0) console.warn(warnings);
 
         if (!computeSizeFn) return;
 
@@ -17,7 +19,7 @@ async function renderDiagram(xml, container, viewer, computeSizeFn) {
         container.style.width = `max(100%,${size.width}px)`;
     } catch (err) {
         container.textContent = err;
-        console.log(err.message, err.warnings);
+        console.error(err.message, err.warnings);
     }
 }
 
@@ -70,13 +72,13 @@ async function renderBpmnDiagram(xml, container) {
 }
 
 async function renderDmnDiagram(xml, container) {
-    const DmnViewer = window.DmnJS;
+    const DmnViewer = window.DmnJSViewer;
     const viewer = new DmnViewer({ container });
 
     renderDiagram(xml, container, viewer, computeDmnDiagramSize);
 }
 
-async function transferXmlToForm(editor) {
+async function exportDataBase64(editor) {
     try {
         const options = { format: true };
         const result = await editor.saveXML(options);
@@ -84,43 +86,48 @@ async function transferXmlToForm(editor) {
         if (xml.length > 0) {
             const encoder = new TextEncoder();
             const data = encoder.encode(xml);
-            const base64 = btoa(String.fromCharCode(...data));
-            jQuery('input[name="plugin_bpmnio_data"]').val(base64);
+            return btoa(String.fromCharCode(...data));
         }
     } catch (err) {
-        console.log(err);
+        console.error(err);
+        return null;
     }
+}
+
+function addFormSubmitListener(editor) {
+    const form = document.getElementById('dw__editform');
+    form.addEventListener('submit', async () => {
+        const data = await exportDataBase64(editor);
+        const field = form.querySelector('input[name="plugin_bpmnio_data"]');
+        if (field && data) {
+            field.value = data;
+        }
+    });
 }
 
 async function renderBpmnEditor(xml, container) {
     const BpmnEditor = window.BpmnJS;
     const editor = new BpmnEditor({ container });
-
-    editor.on("commandStack.changed", () => transferXmlToForm(editor));
-
+    addFormSubmitListener(editor);
     renderDiagram(xml, container, editor, null);
-    transferXmlToForm(editor);
 }
 
 async function renderDmnEditor(xml, container) {
     const DmnEditor = window.DmnJS;
     const editor = new DmnEditor({ container });
-
-    editor.on("commandStack.changed", () => transferXmlToForm(editor));
-
+    addFormSubmitListener(editor);
     renderDiagram(xml, container, editor, null);
-    transferXmlToForm(editor);
 }
 
 function safeRender(tag, type, fn) {
     try {
         const root = jQuery(tag);
-        const containerId = "."+type+"_js_container";
+        const containerId = "." + type + "_js_container";
         const container = root.find(containerId)[0];
         // avoid double rendering
         if (container.children?.length > 0) return;
 
-        const dataId = "."+type+"_js_data";
+        const dataId = "." + type + "_js_data";
         const data = root.find(dataId)[0];
         const xml = extractXml(data.textContent);
 
@@ -132,15 +139,15 @@ function safeRender(tag, type, fn) {
 
 jQuery(document).ready(function () {
     jQuery("div[id^=__bpmn_js_]").each((_, tag) =>
-        safeRender(tag, 'bpmn', renderBpmnDiagram)
+        safeRender(tag, "bpmn", renderBpmnDiagram)
     );
     jQuery("div[id^=__dmn_js_]").each((_, tag) =>
-        safeRender(tag, 'dmn',  renderDmnDiagram)
+        safeRender(tag, "dmn", renderDmnDiagram)
     );
     jQuery("div[id=plugin_bpmnio__bpmn_editor]").each((_, tag) =>
-        safeRender(tag, 'bpmn', renderBpmnEditor)
+        safeRender(tag, "bpmn", renderBpmnEditor)
     );
     jQuery("div[id=plugin_bpmnio__dmn_editor]").each((_, tag) =>
-        safeRender(tag, 'dmn', renderDmnEditor)
+        safeRender(tag, "dmn", renderDmnEditor)
     );
 });
