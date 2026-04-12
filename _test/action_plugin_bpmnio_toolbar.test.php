242a8e5c8fdb14becaae50d3af24fc80cf14b6f4
Jaap de Haan <261428+jdehaan@users.noreply.github.com>
2026-04-12 20:05:36 +0200

    fix: Test coverage (#68)
    * fix: Improve test coverage
    
    * feat: Improve static code checking

---
+++ _test/action_plugin_bpmnio_toolbar.test.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * @group plugin_bpmnio
+ * @group plugins
+ */
+class action_plugin_bpmnio_toolbar_test extends DokuWikiTest {
+
+    protected $pluginsEnabled = array('bpmnio');
+
+    /**
+     * Test that the toolbar plugin can be loaded
+     */
+    public function test_plugin_load() {
+        $plugin = plugin_load('action', 'bpmnio_toolbar');
+        $this->assertNotNull($plugin, 'Toolbar action plugin should be loadable');
+    }
+
+    /**
+     * Test that the toolbar handler adds the picker to event data
+     */
+    public function test_toolbar_handler_adds_picker() {
+        $plugin = plugin_load('action', 'bpmnio_toolbar');
+
+        $data = [];
+        $event = new \dokuwiki\Extension\Event('TOOLBAR_DEFINE', $data);
+
+        $plugin->handleToolbar($event);
+
+        $this->assertCount(1, $data, 'Should add one toolbar entry');
+        $picker = $data[0];
+        $this->assertEquals('picker', $picker['type']);
+        $this->assertArrayHasKey('list', $picker);
+        $this->assertCount(2, $picker['list'], 'Picker should have BPMN and DMN entries');
+
+        // Check BPMN entry
+        $bpmn = $picker['list'][0];
+        $this->assertEquals('format', $bpmn['type']);
+        $this->assertNotEmpty($bpmn['open'], 'BPMN open template should not be empty');
+        $this->assertNotEmpty($bpmn['close'], 'BPMN close template should not be empty');
+        $this->assertStringContainsString('bpmnio', $bpmn['open']);
+
+        // Check DMN entry
+        $dmn = $picker['list'][1];
+        $this->assertEquals('format', $dmn['type']);
+        $this->assertNotEmpty($dmn['open'], 'DMN open template should not be empty');
+        $this->assertNotEmpty($dmn['close'], 'DMN close template should not be empty');
+        $this->assertStringContainsString('bpmnio', $dmn['open']);
+    }
+
+    /**
+     * Test that toolbar icons reference the correct directory
+     */
+    public function test_toolbar_icon_paths() {
+        $plugin = plugin_load('action', 'bpmnio_toolbar');
+
+        $data = [];
+        $event = new \dokuwiki\Extension\Event('TOOLBAR_DEFINE', $data);
+        $plugin->handleToolbar($event);
+
+        $picker = $data[0];
+        $this->assertStringContainsString('bpmnio/images/toolbar/', $picker['icon']);
+        $this->assertStringContainsString('bpmnio/images/toolbar/', $picker['list'][0]['icon']);
+        $this->assertStringContainsString('bpmnio/images/toolbar/', $picker['list'][1]['icon']);
+    }
+
+    /**
+     * Test that BPMN template contains valid XML structure
+     */
+    public function test_bpmn_template_content() {
+        $plugin = plugin_load('action', 'bpmnio_toolbar');
+
+        $data = [];
+        $event = new \dokuwiki\Extension\Event('TOOLBAR_DEFINE', $data);
+        $plugin->handleToolbar($event);
+
+        $bpmn_open = $data[0]['list'][0]['open'];
+        $bpmn_close = $data[0]['list'][0]['close'];
+
+        $this->assertStringContainsString('<bpmnio type="bpmn">', $bpmn_open);
+        $this->assertStringContainsString('</bpmnio>', $bpmn_close);
+    }
+
+    /**
+     * Test that DMN template contains valid XML structure
+     */
+    public function test_dmn_template_content() {
+        $plugin = plugin_load('action', 'bpmnio_toolbar');
+
+        $data = [];
+        $event = new \dokuwiki\Extension\Event('TOOLBAR_DEFINE', $data);
+        $plugin->handleToolbar($event);
+
+        $dmn_open = $data[0]['list'][1]['open'];
+        $dmn_close = $data[0]['list'][1]['close'];
+
+        $this->assertStringContainsString('<bpmnio type="dmn">', $dmn_open);
+        $this->assertStringContainsString('</bpmnio>', $dmn_close);
+    }
+}
--- _test/syntax_plugin_bpmnio_bpmnio.test.php
+++ _test/syntax_plugin_bpmnio_bpmnio.test.php
@@ -22,10 +22,10 @@ class syntax_plugin_bpmnio_test extends DokuWikiTest {
         XML...
         </bpmnio>
         IN;
- 
+
         $instructions = p_get_instructions($input);
         $xhtml = p_render('xhtml', $instructions, $info);
- 
+
         $this->assertEquals($expected, $xhtml);
     }
 
@@ -44,10 +44,170 @@ class syntax_plugin_bpmnio_test extends DokuWikiTest {
         XML...
         </bpmnio>
         IN;
- 
+
         $instructions = p_get_instructions($input);
         $xhtml = p_render('xhtml', $instructions, $info);
- 
+
         $this->assertEquals($expected, $xhtml);
     }
-}
+
+    /**
+     * Test that type defaults to bpmn when not specified
+     */
+    public function test_syntax_default_type() {
+        $info = array();
+
+        $input = <<<IN
+        <bpmnio>
+        XML...
+        </bpmnio>
+        IN;
+
+        $instructions = p_get_instructions($input);
+        $xhtml = p_render('xhtml', $instructions, $info);
+
+        $this->assertStringContainsString('bpmn_js_data', $xhtml);
+        $this->assertStringContainsString('bpmn_js_canvas', $xhtml);
+        $this->assertStringContainsString('bpmn_js_container', $xhtml);
+    }
+
+    /**
+     * Test empty content between tags
+     */
+    public function test_syntax_empty_content() {
+        $info = array();
+
+        $input = <<<IN
+        <bpmnio type="bpmn">
+        </bpmnio>
+        IN;
+
+        $instructions = p_get_instructions($input);
+        $xhtml = p_render('xhtml', $instructions, $info);
+
+        // Should still produce the structure, with base64 of whitespace/empty
+        $this->assertStringContainsString('plugin-bpmnio', $xhtml);
+        $this->assertStringContainsString('bpmn_js_data', $xhtml);
+    }
+
+    /**
+     * Test that multiline XML content is properly base64-encoded
+     */
+    public function test_syntax_multiline_content() {
+        $info = array();
+
+        $input = <<<IN
+        <bpmnio type="bpmn">
+        <?xml version="1.0"?>
+        <definitions xmlns="http://www.omg.org/spec/BPMN/20100524/MODEL">
+        </definitions>
+        </bpmnio>
+        IN;
+
+        $instructions = p_get_instructions($input);
+        $xhtml = p_render('xhtml', $instructions, $info);
+
+        $this->assertStringContainsString('bpmn_js_data', $xhtml);
+        // Verify the data section contains valid base64
+        preg_match('/<div class="bpmn_js_data">\s*(.*?)\s*<\/div>/s', $xhtml, $matches);
+        $this->assertNotEmpty($matches[1]);
+        $decoded = base64_decode(trim($matches[1]), true);
+        $this->assertNotFalse($decoded, 'Content should be valid base64');
+        $this->assertStringContainsString('definitions', $decoded);
+    }
+
+    /**
+     * Test that the plugin produces section edit markers for inline content
+     */
+    public function test_syntax_section_edit_bpmn() {
+        $info = array();
+
+        $input = <<<IN
+        <bpmnio type="bpmn">
+        Content
+        </bpmnio>
+        IN;
+
+        $instructions = p_get_instructions($input);
+        $xhtml = p_render('xhtml', $instructions, $info);
+
+        $this->assertStringContainsString('sectionedit', $xhtml);
+        $this->assertStringContainsString('plugin_bpmnio_bpmn', $xhtml);
+    }
+
+    /**
+     * Test that the plugin produces section edit markers for DMN inline content
+     */
+    public function test_syntax_section_edit_dmn() {
+        $info = array();
+
+        $input = <<<IN
+        <bpmnio type="dmn">
+        Content
+        </bpmnio>
+        IN;
+
+        $instructions = p_get_instructions($input);
+        $xhtml = p_render('xhtml', $instructions, $info);
+
+        $this->assertStringContainsString('sectionedit', $xhtml);
+        $this->assertStringContainsString('plugin_bpmnio_dmn', $xhtml);
+    }
+
+    /**
+     * Test that unrecognized text outside <bpmnio> is not affected
+     */
+    public function test_syntax_no_interference() {
+        $info = array();
+
+        $input = <<<IN
+        Hello World
+        <bpmnio type="bpmn">
+        XML...
+        </bpmnio>
+        Goodbye World
+        IN;
+
+        $instructions = p_get_instructions($input);
+        $xhtml = p_render('xhtml', $instructions, $info);
+
+        $this->assertStringContainsString('Hello World', $xhtml);
+        $this->assertStringContainsString('Goodbye World', $xhtml);
+        $this->assertStringContainsString('plugin-bpmnio', $xhtml);
+    }
+
+    /**
+     * Test the handle method directly for ENTER state
+     */
+    public function test_handle_enter_state() {
+        $plugin = plugin_load('syntax', 'bpmnio_bpmnio');
+        $this->assertNotNull($plugin, 'Plugin should be loadable');
+
+        $handler = new Doku_Handler();
+        $result = $plugin->handle('<bpmnio type="bpmn">', DOKU_LEXER_ENTER, 0, $handler);
+
+        $this->assertEquals(DOKU_LEXER_ENTER, $result[0]);
+        $this->assertEquals('bpmn', $result[1]);
+    }
+
+    /**
+     * Test the handle method directly for EXIT state
+     */
+    public function test_handle_exit_state() {
+        $plugin = plugin_load('syntax', 'bpmnio_bpmnio');
+        $handler = new Doku_Handler();
+        $result = $plugin->handle('</bpmnio>', DOKU_LEXER_EXIT, 0, $handler);
+
+        $this->assertEquals(DOKU_LEXER_EXIT, $result[0]);
+    }
+
+    /**
+     * Test that the plugin is correctly registered
+     */
+    public function test_plugin_registration() {
+        $plugin = plugin_load('syntax', 'bpmnio_bpmnio');
+        $this->assertNotNull($plugin, 'Plugin should be loadable');
+        $this->assertEquals('block', $plugin->getPType());
+        $this->assertEquals('protected', $plugin->getType());
+        $this->assertEquals(0, $plugin->getSort());
+    }
